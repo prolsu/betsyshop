aca3a9ce3ea92c001da15440381a96fa1a158517
Pedro Alburqueque <prolsu88@gmail.com>
2021-01-22 21:09:32 -0500

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring our custom middleware for checking if a user is logged in
-// const isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
@@ -19,22 +19,28 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  // app.get("/home", isAuthenticated, (req, res) => {
-  //   res.render("home");
-  // });
+  app.get("/home", isAuthenticated, (req, res) => {
+    res.render("home");
+  });
 
   app.get("/home", (req, res) => {
     if (req.user) {
       res.redirect("/home");
     }
-    res.render("home");
+    res.render("login");
   });
 
   app.get("/buying", (req, res) => {
-    res.render("buying");
+    if (req.user) {
+      res.render("buying");
+    }
+    res.render("login");
   });
 
   app.get("/selling", (req, res) => {
-    res.render("selling");
+    if (req.user) {
+      res.redirect("/selling/listings");
+    }
+    res.render("login");
   });
 };
